9a9e3dddc60175f1abb8e64284cce75a3fba4994
Hareesh Pilli <hareeshpilli918@gmail.com>
2026-06-23 21:00:23 +0530

    created the components and added routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home-component/home-component';
+import { AboutComponent } from './about-component/about-component';
+import { ErrorComponent } from './error-component/error-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+
+  // wildcard route (404 page)
+  { path: '**', component: ErrorComponent }
+];
